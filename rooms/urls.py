986e24731b6704ece24124ae783aa3aa0dc7bd80
semikite-django <semikite.django@gmail.com>
2022-05-13 21:05:28 +0900

    add Search (FBV)

--- rooms/urls.py
+++ rooms/urls.py
@@ -6,4 +6,5 @@ app_name = 'rooms'
 urlpatterns = [
     # path('<int:pk>/', views.room_detail, name='detail'),
     path('<int:pk>/', views.RoomDetail.as_view(), name='detail'),
+    path('search/', views.search, name='search'),
 ]
